debed3759dcf4962422ebb7e5b69c211d86fd456
gaotiansong <59554946+gaotiansong@users.noreply.github.com>
2020-07-10 20:45:26 +0800

    Update 自动生成文章-生成.py

--- 自动生成文章-生成.py
+++ 自动生成文章-生成.py
@@ -1,6 +1,7 @@
 from random import choice, randint
 from urllib.request import urlopen
-import time
+import time,re
+import csv
 
 def wordListSum(wordList):
     sum=0
@@ -76,38 +77,198 @@ def indexstr(str1,str2):
     return indexstr2[-1]
 
 #生成链长为100的马尔科夫链
-def pr_text():
-    with open(r'D:\Backup\桌面\python文本处理\db.txt') as f:
+def pr_text(keyword,db_path):
+    #打开文本，把内容转换为字典
+    with open(db_path,encoding='utf8') as f:
         r=f.read()
         wordDict=eval(r)
-
-    length=200
-    #抽取一个词作为开始，这个词第一个字母必须为大写
-    while True:
-        s=choice(list(wordDict))
-        if s.istitle():
-            break
-
-    chain=[s]
-    #currentWord=" "
+    
+    length=300
+    #设置装关键词的列表
+    chain=[keyword]
+    #循环length次,每次取一个关键词放入列表，最后组合成句子
+    '''
     for i in range(0,length):
+        print(i)
         try:
             keylist=wordDict[chain[-1]]
         except Exception as e:
             continue
         newWord=retrieveRandomWord(keylist)
         chain.append(newWord)
+    '''
+    n=0
+    while n<length:
+        try:
+            keylist=wordDict[chain[-1]]
+        except Exception as e:
+            print(e)
+            break
+        newWord=retrieveRandomWord(keylist)
+        chain.append(newWord)
+        n=n+1
+    chain=chain[1:]
     s=' '.join(chain)
+
+    #最后一个'.'的位置为一段话的结束，保证句子的完整性。
+    '''
     try:
         n=indexstr(s,'.')
+        #句子要包括最后的'.'
         s=s[:n+1]
     except Exception as e:
-        print(e)
+        print('获取文本S',e,s)
+    '''
+    n=indexstr(s,'.')
+    s=s[:n+1]
     return s
 
-while True:
-    s=pr_text()
-    if len(s)<150:
-        continue
-    print(s+'\n')
-    time.sleep(10)
+def pr_text_b(keyword,db_path):
+    with open(db_path,encoding='utf8') as f:
+        r=f.read()
+        wordDict=eval(r)
+    length=randint(1000,5000)
+    chain=[keyword]
+    for i in range(0,length):
+        try:
+            keylist=wordDict[chain[-1]]
+        except Exception as e:
+            continue
+        newWord=retrieveRandomWord(keylist)
+        if newWord in '!,.?/n:;?':
+            break
+        chain.append(newWord)
+    chain=chain[1:]
+    s=' '.join(chain)
+    return s
+
+#反转字符串
+def s_ab(s):
+    s1=s[::-1]
+    return s1
+
+#获取关键词的头尾词
+def h_t(s):
+    s1=s.split(' ')
+    s_a=s1[0]
+    s_b=s1[-1]
+    return s_a,s_b
+
+#得出拼接好的文本
+def writing(keyword):
+    ka,kb=h_t(keyword)
+    #得出前半截文本
+    s1=pr_text_b(s_ab(ka),dbB)
+    s1=s_ab(s1)
+    #得出后半截文本
+    s2=pr_text(kb,dbA)
+    #拼接成完整文本
+    txt=s1+' '+keyword+' '+s2
+    return txt
+
+
+def to_html(d,tem):
+    with open(tem,"r") as f:
+        t=f.read()
+        html=t.format_map(d)
+        return html
+
+def write_in(s,path):
+    with open(path,'w') as f:
+        f.write(s)
+
+def write_page(dbA,dbB,keyword):
+    txt=writing(keyword)
+    
+    #如果文本小于某个随机数
+    while len(txt)<randint(100,1000):
+        txt=writing(keyword)
+
+    while len(txt)<1000:
+        #print(len(txt))
+        txt=txt+'<br><br>'+writing(keyword)
+        #print(len(txt))
+
+    d={'title':keyword,
+        'h1':keyword,
+        'content':txt,
+        'context':'这是next',
+        'relevant':'这是相关'
+        }
+    return d
+
+#把内容写入wordpress
+def wt_wordpress(name,des):
+    from wordpress_xmlrpc import Client, WordPressPost
+    from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+    from wordpress_xmlrpc.methods.users import GetUserInfo
+    from wordpress_xmlrpc.methods import posts
+    from wordpress_xmlrpc.methods import taxonomies
+    from wordpress_xmlrpc import WordPressTerm
+    from wordpress_xmlrpc.compat import xmlrpc_client
+    from wordpress_xmlrpc.methods import media, posts
+    import sys
+    import importlib
+    import random
+     
+    importlib.reload(sys)
+     
+    post = WordPressPost()
+    post.title = name
+    post.content = des
+    post.post_status = 'publish' #文章状态，不写默认是草稿，private表示私密的，draft表示草稿，publish表示发布
+    wp = Client('https://customp.cn/xmlrpc.php', 'customp', 'PYOqDvyjw2F6vQGir%pnd1PQ') 
+    '''
+    post.terms_names = { 
+        'post_tag':seokey(key_tag,30,3), #文章所属标签，没有则自动创建
+        'category':seokey(key_category,20000,2) #文章所属分类，没有则自动创建
+        }
+    '''
+ 
+    post.custom_fields = []   #自定义字段列表
+    post.custom_fields.append({  #添加一个自定义字段
+        'key': 'price',
+        'value': 3
+        })
+ 
+    post.custom_fields.append({ #添加第二个自定义字段
+        'key': 'ok',
+        'value': 'customfw.com'
+        })
+    post.id = wp.call(posts.NewPost(post))
+    time.sleep(1)
+
+#设置文本模型库
+dbA=r'D:\Backup\桌面\python文本处理\am\db-a.txt'
+dbB=r'D:\Backup\桌面\python文本处理\am\db-b.txt'
+#设置html模板
+tem=r"D:\Backup\桌面\\python文本处理\a\a.txt"
+#设置内容生成路径
+html_path=r"D:\Backup\桌面\\python文本处理\\a\\"
+
+#设置关键词列表
+keywords=r'C:\Users\MyPC\Downloads\pillow-phrase_related-us.csv'
+
+with open(keywords,encoding='utf8') as f:
+    ks=csv.reader(f)
+    n=1
+    k_old=''
+    for k in ks:
+        if k[0] not in '关键词' and len(k[0])<=30:
+            keyword=k[0]
+            keyword=re.sub('\?|\!|\.','',keyword)
+            #print(keyword)
+            #生成内容主体
+            try:
+                d=write_page(dbA,dbB,keyword)
+            except Exception as e:
+                print(e,'生成内容失败')
+                continue
+            name=d['h1']
+            des=d['content']
+            try:
+                wt_wordpress(name,des)
+            except Exception as e:
+                print(e,'写入失败')
+            print(n,'篇')
+            n=n+1
