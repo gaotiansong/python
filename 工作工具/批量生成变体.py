c07e06be85d577356f791c41772c4ddacdd00530
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-10-30 20:29:02 +0800

    Update 批量生成变体.py
    自动判断是美国还是英国的表格

--- 工作工具/批量生成变体.py
+++ 工作工具/批量生成变体.py
@@ -6,40 +6,86 @@ def Wt_csv(path,f):
         writer=csv.writer(w)
         writer.writerow(f)
 
+def pro_uk(old_path,mvar,m1var):
+    new_path=old_path.split(r".")
+    new_path=new_path[0]+r"New.csv"
+    with open(old_path,"r") as f:
+        rows=csv.reader(f)
+        n=0
+        parent_sku=""
+        for row in rows:
+            if  "EAN" not in row:
+                Wt_csv(new_path,row)
+                continue
+            #判断是不是母体
+            if "Parent" in row and n==0:
+                parent_sku=row[1]
+                print(row[1])
+                Wt_csv(new_path,row)
+            #判断是不是变量
+            if "Child" in row and "Variation" in row:
+                row[25]=parent_sku
+                print(row[1],row[25])
+                Wt_csv(new_path,row)
+                n=n+1
+                if n==mvar:
+                    for i in range(m1var):
+                        row[1]=row[1]+"01"
+                        row[4]="需要EAN"
+                        row[12]="组合价格"
+                        Wt_csv(new_path,row)
+                    n=0
+                    parent_sku=""
 
-#old_path=r"/Users/gaotiansong/Desktop/变体处理/SI23Z8AW_20211029_001.csv"
+def pro_us(old_path,mvar,m1var):
+    new_path=old_path.split(r".")
+    new_path=new_path[0]+r"New.csv"
+    with open(old_path,"r") as f:
+        rows=csv.reader(f)
+        n=0
+        parent_sku=""
+        for row in rows:
+            if  row[6]!="EAN":
+                Wt_csv(new_path,row)
+                continue
+            if row[5]=="" and n==0:
+                parent_sku=row[1]
+                print(row[1])
+                Wt_csv(new_path,row)
+            if row[5]!="":
+                row[23]=parent_sku
+                print(row[1],row[23])
+                Wt_csv(new_path,row)
+                n=n+1
+                if n==mvar:
+                    for i in range(m1var):
+                        row[1]=row[1]+"01"
+                        row[5]="需要EAN"
+                        row[10]="组合价格"
+                        Wt_csv(new_path,row)
+                    n=0
+                    parent_sku=""
+
 old_path=input("输入要处理的文件:")
 old_path=old_path.strip()
 old_path=old_path.replace(r'"','')
-new_path=old_path.split(r".")
-new_path=new_path[0]+r"New.csv"
-#mvar=2 #变体个数
+
 mvar=input("变体个数:")
 mvar=int(mvar)
-
 m1var=1 #祖合变体个数
-
+#判断是美国还是英国 然后调用相应的函数
 with open(old_path,"r") as f:
     rows=csv.reader(f)
     n=0
-    parent_sku=""
+    cou=""
     for row in rows:
-        if  row[6]!="EAN":
-            Wt_csv(new_path,row)
-            continue
-        if row[5]=="" and n==0:
-            parent_sku=row[1]
-            print(row[1])
-            Wt_csv(new_path,row)
-        if row[5]!="":
-            row[23]=parent_sku
-            print(row[1],row[23])
-            Wt_csv(new_path,row)
-            n=n+1
-            if n==mvar:
-                for i in range(m1var):
-                    row[1]=row[1]+"01"
-                    row[5]="需要EAN"
-                    Wt_csv(new_path,row)
-                n=0
-                parent_sku=""
+        if n==3:
+            cou=row[0]
+            break
+        n=n+1
+if cou=="stringlight":
+    print("英国")
+    pro_uk(old_path,mvar,m1var)
+elif cou=="hangingornament":
+    print("美国")
+    pro_us(old_path,mvar,m1var)
