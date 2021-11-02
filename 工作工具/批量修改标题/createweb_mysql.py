3844562776094e8ab0ee42e9aaea2446e740e84e
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-11-02 15:53:19 +0800

    Update createweb_mysql.py
    显示加载数据过程

--- 工作工具/批量修改标题/createweb_mysql.py
+++ 工作工具/批量修改标题/createweb_mysql.py
@@ -31,73 +31,75 @@ def find_datas(tablename):
     datas=list(set(datas))
     return datas
 
-#获取各种关键词
-keywords=find_datas(tablename="keyword")
+if __name__=="__main__":
+    #获取各种关键词
+    print("加载在线数据中")
+    keywords=find_datas(tablename="keyword")
+    print("成功加载keywords")
+    add1s=find_datas(tablename="add1")
+    add1=sample(add1s,1)[0]
+    print("成功加载add1")
 
-add1s=find_datas(tablename="add1")
-add1=sample(add1s,1)[0]
+    add2s=find_datas(tablename="add2")
+    add2=sample(add2s,1)[0]
+    print("成功加载add3")
+
+    zt1s=find_datas(tablename="zt")
+    zt1=sample(zt1s,1)[0]
+    print("成功加载zt1s")
+
+    ch1s=find_datas(tablename="ch")
+    ch1=sample(ch1s,1)[0]
+    print("成功加载ch1s")
+
+    pro_tath=input("输入你要处理的文件:")
+    pro_tath=pro_tath.strip()
+
+    custom=input("输入固定标题（可为空回车跳过）:")
 
 
-add2s=find_datas(tablename="add2")
-add2=sample(add2s,1)[0]
+    with open(pro_tath,"r") as f:
+        pros=csv.reader(f)
+        for p in pros:
+            if p[0]=="Seller SKU" or p[0]=="item_sku":
+                continue
+            print("p1",p[1])
+            ls=[]
+            title=""
+            while True:
+                ls1=ls
+                s1=" ".join(ls1)
+                ls.append(sample(keywords,1)[0])
+                lss=[p[1]]+["Christmas ornaments"]+ls+[add1]+[add2]+[zt1]+[custom]+["For"]+[ch1]
+                s=" ".join(lss)
+                ns=[140,160,180]
+                if len(s)>sample(ns,1)[0] and len(s)<200:
+                    print("s")
+                    title=s
+                    break
+                elif len(s)>200:
+                    print("s1")
+                    if len(s1)<80:
+                        continue
+                    title=s1
+                    break
+            print(len(title),title.title())
+            newpro_path=pro_tath.split(r".")
+            newpro_path=newpro_path[0]+r"New.csv"
+            h1=title.title().strip()
+            h1=re.sub("(\s)+"," ",h1)
 
+            while True:
+                lstags=[]
+                lstag=sample(keywords,5)
+                lstags1=lstags
+                lstags=lstags+lstag
+                keys=",".join(list(set(lstags)))
+                if len(keys)>200 and len(keys)<250:
+                    break
+                elif len(keys)>250:
+                    keys=",".join(lstags1)
 
-zt1s=find_datas(tablename="zt")
-zt1=sample(zt1s,1)[0]
-
-
-ch1s=find_datas(tablename="ch")
-ch1=sample(ch1s,1)[0]
-
-
-pro_tath=input("输入你要处理的文件:")
-pro_tath=pro_tath.strip()
-
-custom=input("输入固定标题（可为空回车跳过）:")
-
-
-with open(pro_tath,"r") as f:
-    pros=csv.reader(f)
-    for p in pros:
-        if p[0]=="Seller SKU" or p[0]=="item_sku":
-            continue
-        print("p1",p[1])
-        ls=[]
-        title=""
-        while True:
-            ls1=ls
-            s1=" ".join(ls1)
-            ls.append(sample(keywords,1)[0])
-            lss=[p[1]]+["Christmas ornaments"]+ls+[add1]+[add2]+[zt1]+[custom]+["For"]+[ch1]
-            s=" ".join(lss)
-            ns=[140,160,180]
-            if len(s)>sample(ns,1)[0] and len(s)<200:
-                print("s")
-                title=s
-                break
-            elif len(s)>200:
-                print("s1")
-                if len(s1)<80:
-                    continue
-                title=s1
-                break
-        print(len(title),title.title())
-        newpro_path=pro_tath.split(r".")
-        newpro_path=newpro_path[0]+r"New.csv"
-        h1=title.title().strip()
-        h1=re.sub("(\s)+"," ",h1)
-
-        while True:
-            lstags=[]
-            lstag=sample(keywords,5)
-            lstags1=lstags
-            lstags=lstags+lstag
-            keys=",".join(list(set(lstags)))
-            if len(keys)>200 and len(keys)<250:
-                break
-            elif len(keys)>250:
-                keys=",".join(lstags1)
-
-        with open(newpro_path,"a",newline="") as w:
-            writer=csv.writer(w)
-            writer.writerow([p[0],p[1],h1,keys])
+            with open(newpro_path,"a",newline="") as w:
+                writer=csv.writer(w)
+                writer.writerow([p[0],p[1],h1,keys])
