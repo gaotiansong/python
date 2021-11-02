b7bcac6453f531095b4118ee5e883c52e409d0e7
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-11-02 13:08:24 +0800

    Update 批量生成变体.py
    1.用列表实现对列
    2.删除用过的ean

--- 工作工具/批量生成变体.py
+++ 工作工具/批量生成变体.py
@@ -1,20 +1,18 @@
 #coding=utf-8
 import csv
-from multiprocessing import Queue
-
-def wt_q(q,ena_path):
+def wt_q(asins,ena_path):
     #给对列写入ean
     with open(ena_path,"r") as f:
         rows=f.readlines()
         for row in rows:
-            q.put(str(row.strip()))
+            asins.append(str(row.strip()))
 
 def Wt_csv(path,f):
     with open(path,"a",newline="") as w:
         writer=csv.writer(w)
         writer.writerow(f)
 
-def pro_uk(old_path,mvar,m1var,q):
+def pro_uk(old_path,mvar,m1var,asins):
     new_path=old_path.split(r".")
     new_path=new_path[0]+r"New.csv"
     with open(old_path,"r") as f:
@@ -39,13 +37,13 @@ def pro_uk(old_path,mvar,m1var,q):
                 if n==mvar:
                     for i in range(m1var):
                         row[1]=row[1]+"01"
-                        row[4]=str(q.get()) #写入ean
+                        row[4]=asins.pop()
                         row[12]="组合价格"
                         Wt_csv(new_path,row)
                     n=0
                     parent_sku=""
 
-def pro_us(old_path,mvar,m1var,q):
+def pro_us(old_path,mvar,m1var,asins):
     new_path=old_path.split(r".")
     new_path=new_path[0]+r"New.csv"
     with open(old_path,"r") as f:
@@ -68,39 +66,48 @@ def pro_us(old_path,mvar,m1var,q):
                 if n==mvar:
                     for i in range(m1var):
                         row[1]=row[1]+"01"
-                        row[5]=str(q.get()) #写入ean
+                        row[5]=asins.pop()
                         row[10]="组合价格"
                         Wt_csv(new_path,row)
                     n=0
                     parent_sku=""
 
-old_path=input("输入要处理的文件:")
-old_path=old_path.strip()
-old_path=old_path.replace(r'"','')
+if __name__=="__main__":
+    old_path=input("输入要处理的文件:")
+    old_path=old_path.strip()
+    old_path=old_path.replace(r'"','')
 
-q=Queue(1000)
-#ena_path=r"/Users/gaotiansong/Desktop/变体处理/ean.csv"
-ena_path=input("输入ean文件:")
-ena_path=ena_path.strip()
-ena_path=ena_path.replace(r'"','')
-wt_q(q,ena_path)
-
-mvar=input("变体个数:")
-mvar=int(mvar)
-m1var=1 #祖合变体个数
-#判断是美国还是英国 然后调用相应的函数
-with open(old_path,"r") as f:
-    rows=csv.reader(f)
-    n=0
-    cou=""
-    for row in rows:
-        if n==3:
-            cou=row[0]
-            break
-        n=n+1
-if cou=="stringlight":
-    print("英国")
-    pro_uk(old_path,mvar,m1var,q)
-elif cou=="hangingornament":
-    print("美国")
-    pro_us(old_path,mvar,m1var,q)
+    asins=[]
+    ena_path=input("输入ean文件:")
+    ena_path=ena_path.strip()
+    ena_path=ena_path.replace(r'"','')
+    wt_q(asins,ena_path)
+    asins=asins[::-1]
+    mvar=input("变体个数:")
+    mvar=int(mvar)
+    m1var=1 #祖合变体个数
+    #判断是美国还是英国 然后调用相应的函数
+    with open(old_path,"r") as f:
+        rows=csv.reader(f)
+        n=0
+        cou=""
+        for row in rows:
+            if n==3:
+                cou=row[0]
+                break
+            n=n+1
+    if cou=="stringlight":
+        print("英国")
+        pro_uk(old_path,mvar,m1var,asins)
+    elif cou=="hangingornament":
+        print("美国")
+        pro_us(old_path,mvar,m1var,asins)
+    
+    asin_new=[]
+    for i in asins:
+        asin_new.append([i])
+    new_ean=ena_path.split(r".")
+    new_ean=new_ean[0]+r"剩余.txt"
+    with open(new_ean,"w",encoding="utf-8",newline="") as f:
+        writer=csv.writer(f)
+        writer.writerows(asin_new)
