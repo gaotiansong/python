b86de2618bdddd750a67617d4aa935f443cadb72
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-12-02 16:02:57 +0800

    Create 计算字符串所有相邻组合.py

---
+++ 字符串处理/计算字符串所有相邻组合.py
@@ -0,0 +1,15 @@
+#给它字符串 "gao tian song"
+#输出 ["gao","tian","song","gao tian","tian song","gao tian song"]
+
+def find_words(s):
+    #计算一段文字所有相邻组合
+    words=[]
+    s=re.sub("\s+"," ",s)
+    s_ls=s.split(" ")
+    size=len(s_ls)
+    for i in range(size):
+        i=i+1
+        for i1 in range(size):
+            if i1+i<=size:
+                words.append(" ".join(s_ls[i1:i1+i]))
+    return words
