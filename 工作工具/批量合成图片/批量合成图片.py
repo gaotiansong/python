f30e41c7f0571b90abda64108742a3ec84246fbc
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-12-24 12:00:44 +0800

    Update 批量合成图片.py

--- 工作工具/批量合成图片/批量合成图片.py
+++ 工作工具/批量合成图片/批量合成图片.py
@@ -1,17 +1,19 @@
 from PIL import Image
 import os
 import random
+import time
 
 
 def custom_pic(path1, path2, pic, save_path):
+    print("开始执行合成函数")
     custom_colour = ""
 
     img1 = Image.open(path1)
-    img1 = img1.convert('RGBA')
+    img1 = img1.convert('RGB')
     # img1 = img1.resize((2000, 2000))  # 注意，是2个括号
 
     img2 = Image.open(path2)
-    img2 = img2.convert('RGBA')
+    img2 = img2.convert('RGB')
     # img2 = img2.resize((1000, 1000))
     img_array = img2.load()
 
@@ -66,6 +68,7 @@ def custom_pic(path1, path2, pic, save_path):
     # img1.show()
     img1.save(save_path + "/" + pic)
     print("成功合成:", pic)
+    time.sleep(3)
 
 
 def get_w_h(img_path):
@@ -90,10 +93,14 @@ if __name__ == "__main__":
         path1 = path1_dir + "/" + back
 
         path2 = path2_dir + "/" + pic
+        # n1 用来表示张图片的比例相似度
         n1 = abs(get_w_h(path2) - get_w_h(path1))
-        while n1 > 0.001:
+        # n 用来表示查找背景的最大次数 如果查找100找不到适合的背景 ，就不考虑相似度了
+        n=0
+        while n1 > 0.001 and n< 100:
+            n=n+1
             back = random.sample(os.listdir(path1_dir), 1)[0]
             path1 = path1_dir + "/" + back
             n1 = abs(get_w_h(path2) - get_w_h(path1))
-
+            print("n1:",n1)
         custom_pic(path1, path2, pic, save_path)
