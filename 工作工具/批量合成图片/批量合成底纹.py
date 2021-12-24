af99054a3a3e57e655d8691a581b7824398de935
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2021-12-25 00:28:45 +0800

    Update and rename 批量合成图片.py to 批量合成底纹.py
    多进程版

--- 工作工具/批量合成图片/批量合成底纹.py
+++ 工作工具/批量合成图片/批量合成底纹.py
@@ -3,6 +3,7 @@ import os
 import random
 import time
 import re
+import multiprocessing as mp
 
 
 def custom_pic(path1, path2, pic, save_path):
@@ -34,7 +35,7 @@ def custom_pic(path1, path2, pic, save_path):
             img2 = img2.resize((int(w2), int(h2)))
     # 长或者宽比背景大
     elif width2 > width1 or height2 > height1:
-        # 元素都宽比背景都宽大
+        # 元素的宽比背景的宽大
         if width2 > width1:
             w2 = width1
             p = width1 / width2
@@ -65,21 +66,51 @@ def custom_pic(path1, path2, pic, save_path):
     y = int(height1 / 2 - height2 / 2)
     img1.paste(img2, (x, y), mask=img2)
     # img1.show()
-    pic = re.sub(r".jpg",r".png",pic)
+    pic = re.sub(r".jpg", r".png", pic)
     img1.save(save_path + "/" + pic)
     print("成功合成:", pic)
     time.sleep(3)
 
 
+# 获取图片的宽比高的比例
 def get_w_h(img_path):
     img = Image.open(img_path)
     w, h = img.size
     return w / h
 
 
-if __name__ == "__main__":
+# 工作函数
+def working(pic, path1_dir, path2_dir, save_path):
+    path2 = path2_dir + "/" + pic
+    n1 = 100
+    n2 = 0.01
+    while n1 > n2:
+        # 从所有底纹文件夹中随机抽取一个文件夹
+        backs = random.sample(os.listdir(path1_dir), 5)
+        for back in backs:
+            try:
+                float(back)
+            except Exception as e:
+                print(e)
+                continue
+            n1 = abs(get_w_h(path2) - float(back))
+            if n1 < n2:
+                print("差异度：", n1)
+                # 差异度不超过某个值时就可以选择该文件夹 然后从该文件夹中随机抽取一张图片
+                back_pic_path = path1_dir + r"/" + back  # 具体规格的底纹图文件夹
+                # 从该规格的底纹文件夹中随机抽取一张图片
+                back_pic = random.sample(os.listdir(back_pic_path), 1)[0]
+                path1 = back_pic_path + "/" + back_pic
+                custom_pic(path1, path2, pic, save_path)
+                break
 
-    dir1 = input("请输入背景图文件夹地址:")
+
+if __name__ == "__main__":
+    from multiprocessing import Pool
+    import queue
+
+    q = queue.Queue()
+    dir1 = input("请输入底纹文件夹地址:")
     dir2 = input("请输入素材文件夹地址:")
     path1_dir = dir1.strip()
     path2_dir = dir2.strip()
@@ -89,18 +120,10 @@ if __name__ == "__main__":
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     for pic in os.listdir(path2_dir):
-        back = random.sample(os.listdir(path1_dir), 1)[0]
-        path1 = path1_dir + "/" + back
-
-        path2 = path2_dir + "/" + pic
-        # n1 用来表示张图片的比例相似度
-        n1 = abs(get_w_h(path2) - get_w_h(path1))
-        # n 用来表示查找背景的最大次数 如果查找100找不到适合的背景 ，就不考虑相似度了
-        n=0
-        while n1 > 0.001 and n< 100:
-            n=n+1
-            back = random.sample(os.listdir(path1_dir), 1)[0]
-            path1 = path1_dir + "/" + back
-            n1 = abs(get_w_h(path2) - get_w_h(path1))
-            print("寻找底纹",n1)
-        custom_pic(path1, path2, pic, save_path)
+        q.put(pic)
+    pool = Pool(processes=10)
+    while not q.empty():
+        # 只要队列不空 一直提交进程执行
+        pool.apply_async(working, args=(q.get(), path1_dir, path2_dir, save_path))
+    pool.close()
+    pool.join()
