38e314e80d51e84754f5ace8a35a40628f7dd3a6
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2022-01-05 21:37:17 +0800

    Update main.py

--- 工作工具/清理违规词/main.py
+++ 工作工具/清理违规词/main.py
@@ -1,14 +1,15 @@
 import os
 import csv
 import sys
-import enchant
 from pathlib import Path
 import re
 
-def wt_csv(path,data):
-    with open(path,"a",newline="") as f:
-        writer=csv.writer(f)
-        writer.writerow(data)
+
+def wt_csv(path, data):
+    with open(path, "a", newline="") as f:
+        wt = csv.writer(f)
+        wt.writerow(data)
+
 
 # 遍历文件夹下文件方法
 def get_file_list(p, file_list):
@@ -25,6 +26,7 @@ def get_file_list(p, file_list):
         else:
             get_file_list(p + f, file_list)
     return file_list
+
 
 # DFA过滤敏感词算法
 class DFAFilter(object):
@@ -76,7 +78,7 @@ class DFAFilter(object):
                         start += step_ins - 1
                         break
                 else:
-                    ret.append(message.split(" ")[start]+" ")
+                    ret.append(message.split(" ")[start] + " ")
                     break
             else:
                 ret.append(message.split(" ")[start])
@@ -84,9 +86,14 @@ class DFAFilter(object):
 
         return ''.join(ret)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    import enchant
     # 拼写检测 设置语种
     checker = enchant.Dict("en_US")
+    print(checker.check("hello"))
+    print(enchant.list_languages())
+
     maxInt = sys.maxsize
     # CSV 文件读取最大值设置
     while True:
@@ -94,30 +101,24 @@ if __name__=="__main__":
             csv.field_size_limit(maxInt)
             break
         except OverflowError:
-            maxInt = int(maxInt/10)
-    # 遍历出来的文件列表
-    file_list_ = []
+            maxInt = int(maxInt / 10)
 
-    result = {}
-
-    #要处理的文件列表
-    old_path=r"C:\Users\ZG\Desktop\EVERWK"
-    input_old_path=input("输入要处理的文件夹：")
-    if input_old_path!="":
-        old_path=input_old_path
-    old_path=old_path.replace(r'"','')
-    #结果保存地址
-    new_path=old_path+"_New"
-    #new_path=r"C:\Users\ZG\Desktop\EVERWK_New"
+    # 要处理的文件列表
+    old_path = r"D:\Backup\桌面\220105"
+    input_old_path = input("输入要处理的文件夹：")
+    if input_old_path != "":
+        old_path = input_old_path
+    old_path = old_path.replace(r'"', '')
+    # 结果保存地址
+    new_path = old_path + "_New"
     if os.path.exists(new_path):
         pass
     else:
-        os.mkdir(new_path) 
-    #侵权词列表
-    #infringing_path=r'C:\Users\ZG\Desktop\changetitle\侵权词删除\infringing_word_list.txt'
-    infringing_path=r'./infringing_word_list.txt'
+        os.mkdir(new_path)
+    # 侵权词列表
+    infringing_path = r'./infringing_word_list.txt'
 
-    #侵权词列表
+    # 侵权词列表
     with open(infringing_path, 'r', encoding='utf-8') as fl:
         data = fl.readlines()
     infringing_word_list = list(data)
@@ -126,12 +127,16 @@ if __name__=="__main__":
     gfw = DFAFilter()
     gfw.parse()
 
+    # 遍历出来的文件列表
+    file_list_ = []
+    result = {}
     # 获取文件列表
     get_file_list(old_path, file_list_)
-    print("要处理的文件:",file_list_)
+    print("要处理的文件:", file_list_)
     # 遍历文件
     for file in file_list_:
         # 提取店铺名
+        print("file=", file)
         shop = "shop_list[key]"
         # 提取站点 即获取 All+Listings+HNTGJRT-ca.txt 中 的 ca
         region = file["file"][-6:-4].lower()
@@ -153,13 +158,13 @@ if __name__=="__main__":
             # 遍历该站点all_listing文件
             for l in result[key][region]['Listings']:
                 # 结果保存地址
-                new_path_file=os.path.join(new_path,l['file'] +" update.csv")
-                print("保存地址:",new_path_file)
+                new_path_file = os.path.join(new_path, l['file'] + " update.csv")
+                print("保存地址:", new_path_file)
                 # 结果已存在跳过
                 if os.path.exists(new_path_file):
                     print('%s\t%s\t%s\t%s\t%s' % (key, region, l['path'], l['file'], new_path_file))
                     continue
-                #all_listing文件地址
+                # all_listing文件地址
                 listings_file_ = l['path'] + l['file']
                 # 读取 all_listing 文件
                 try:
@@ -167,33 +172,44 @@ if __name__=="__main__":
                         listing_list = csv.reader(fl, delimiter='\t')
                         listing_list = list(listing_list)
                     fl.close()
-                except:
+                except Exception as e:
+                    print(e)
                     with open(listings_file_, 'r', errors='ignore') as fl:
-                        listing_list = csv.reader((line.replace('\x00','') for line in fl), delimiter='\t')
+                        listing_list = csv.reader((line.replace('\x00', '') for line in fl), delimiter='\t')
                         listing_list = list(listing_list)
                     fl.close()
                 # 英语站点处理
                 if region in ['us', 'ca', 'uk']:
                     for listing in listing_list[1:]:
-                        # 标题拼写检测
-                        word_list = listing[0].split(' ')
+                        # listing 表格中的一行
+                        print("listing===", listing)
+                        # 标题拼写检测 word_list 是组成标题的单词列表
+                        print("处理前标题:",listing[0])
+                        # 把标题中的逗号变成空格
+                        title = re.sub(","," ",listing[0])
+                        word_list = title.split(' ')
+                        print("word_list=",word_list)
                         new_title = ''
+                        # 取出标题中的每一个单词进行检查
                         for word in word_list:
                             word_replace = word.replace(',', '')
+                            print("要检查的词：",word_replace)
                             if word_replace:
                                 if checker.check(word_replace):
+                                    # 把处理后的词再次组成句子
+                                    print("获取一个词",word)
                                     new_title = new_title + word + ' '
-
-                        # print(new_title)
+                        print("拼写检查后的标题:",new_title)
                         # 标题侵权词过滤
                         new_title = gfw.filter(new_title)
-                        print(new_title)
                         listing[0] = new_title
+                        print("处理后的标题：",new_title)
 
                         # 描述拼写检测
                         word_list = listing[1].split(' ')
                         new_description = ''
                         for word in word_list:
+                            # 删除关键词中的逗号
                             word_replace = word.replace(',', '')
                             if word_replace:
                                 if checker.check(word_replace):
@@ -208,11 +224,10 @@ if __name__=="__main__":
                 with open(new_path_file, 'w', newline='', encoding='utf-8') as f:
                     writer = csv.writer(f)
                     writer.writerows(listing_list)
-                print('%s\t%s\t%s\t%s\t%s'%(key,region,l['path'],l['file'],new_path_file))
-                #保存成功结果 successfull
-                successfull_file=os.path.join(new_path,"successfull.csv")
-                wt_csv(successfull_file,[l['file']])
+                print('%s\t%s\t%s\t%s\t%s' % (key, region, l['path'], l['file'], new_path_file))
+                # 保存成功结果 successfull
+                successfull = os.path.join(new_path, "successfull.csv")
+                wt_csv(successfull, [l['file']])
             # 保存
-        # print()
     print("成功处理文件夹{}".format(file_list_))
-    _=input("按任意键退出")
+    _ = input("按任意键退出")
