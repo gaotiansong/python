76c6a06ac3505bc3d5c230dc79242e81ed0975a9
哲学家 <59554946+gaotiansong@users.noreply.github.com>
2022-02-21 11:24:52 +0800

    Update ReadExec.py

--- 工作工具/可视化修改亚马逊表格/ReadExec.py
+++ 工作工具/可视化修改亚马逊表格/ReadExec.py
@@ -1,24 +1,26 @@
 # coding=utf-8
-import openpyxl
-from openpyxl import load_workbook
-class ReadExec():
-    path=""
+from openpyxl import load_workbook,Workbook
+
+
+class ReadExec:
+    path = ""
     try:
         wb = load_workbook(path)
         my_sheet = wb["Template"]
     except Exception as e:
-        wb=openpyxl.Workbook()
+        wb = Workbook()
         my_sheet = wb.active
         my_sheet.title = "Template"
         print(e)
     # 确定标题和图片所在列
-    c_name=0
-    c_image1=0
-    c_image2=0
-    c_image3=0
-    max_r=my_sheet.max_row
-    def open_excl(self,path):
-        self.path=path
+    c_name = 0
+    c_image1 = 0
+    c_image2 = 0
+    c_image3 = 0
+    max_r = my_sheet.max_row
+
+    def open_excl(self, path):
+        self.path = path
         self.wb = load_workbook(path)
         self.my_sheet = self.wb["Template"]
         for c in range(1, self.my_sheet.max_column):
@@ -30,20 +32,20 @@ class ReadExec():
                 self.c_image2 = c
             if self.my_sheet.cell(3, c).value == "other_image_url2":
                 self.c_image3 = c
-        self.max_r=self.my_sheet.max_row
+        self.max_r = self.my_sheet.max_row
 
-    def get_image(self,n):
+    def get_image(self, n):
         print("go get_image")
-        n=n+3 #从第四行开始
-        url=self.my_sheet.cell(n, self.c_image1).value
-        name=self.my_sheet.cell(n, self.c_name).value
-        print("url=",url)
+        n = n + 3  # 从第四行开始
+        url = self.my_sheet.cell(n, self.c_image1).value
+        name = self.my_sheet.cell(n, self.c_name).value
+        print("url=", url)
         print("ok get_image")
-        return url,name
+        return url, name
 
-    def sava_f(self,n,tx):
-        n = n + 3 #从第四行开始
+    def sava_f(self, n, tx):
+        n = n + 3  # 从第四行开始
         # 给格子赋值
-        self.my_sheet.cell(n, self.c_name).value=tx
-        print("保存=",tx)
+        self.my_sheet.cell(n, self.c_name).value = tx
+        print("保存=", tx)
         self.wb.save(self.path)
